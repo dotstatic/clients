8d6b139a2ae7808800964bb71fbe04d006607b50
Jake Fink <jfink@bitwarden.com>
2024-01-30 14:09:45 -0500

    serialize login data for cache

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -303,6 +303,7 @@ import { ModalService } from "./modal.service";
         PolicyServiceAbstraction,
         DeviceTrustCryptoServiceAbstraction,
         AuthRequestCryptoServiceAbstraction,
+        StateProvider,
       ],
     },
     {
